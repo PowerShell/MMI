1288a79bb0b6bb7472bc9e36aa7ba64aaae327cd
Aditya Patwardhan <adityap@microsoft.com>
2019-06-26 17:35:13 -0700

    Add named log file

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 mkdir -p test/Microsoft.Management.Infrastructure.Tests/bin/
 cp /opt/omi/lib/libmi.so test/Microsoft.Management.Infrastructure.Tests/bin/
 /opt/omi/bin/omiserver -i -d --livetime 120 --httpport 0 --httpsport 0
-PATH=$PATH:~/.dotnet dotnet test test/Microsoft.Management.Infrastructure.Tests/ -c Linux -o bin
+PATH=$PATH:~/.dotnet dotnet test test/Microsoft.Management.Infrastructure.Tests/ -c Linux -o bin --logger "trx;LogFileName=TestResults.trx"
