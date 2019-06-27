2b53660446b8aa2529b997c9e781a5fe05caa7ad
Aditya Patwardhan <adityap@microsoft.com>
2019-06-26 17:49:06 -0700

    Add artifact publish and execute tests

--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@
 #cp src/omi/Unix/output/lib/libmi.so bin
 
 # Install OMI
-wget https://github.com/microsoft/omi/releases/download/v1.6.0/omi-1.6.0-0.ssl_100.ulinux.x64.deb -O omi-1.6.0-0.ssl_100.ulinux.x64.deb
+wget https://github.com/microsoft/omi/releases/download/v1.6.0/omi-1.6.0-0.ssl_110.ulinux.x64.deb -O omi-1.6.0-0.ssl_110.ulinux.x64.deb
 sudo apt-get install cron -y
-sudo apt install ./omi-1.6.0-0.ssl_100.ulinux.x64.deb
+sudo apt install ./omi-1.6.0-0.ssl_110.ulinux.x64.deb
 
 # Build MMI
 PATH=$PATH:~/.dotnet dotnet build -f netstandard2.0 src/Microsoft.Management.Infrastructure/ -c Linux -o bin
